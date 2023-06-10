3c611aa5b5e22895bc014f0384c6bf6832184159
Jader18 <mendozajader18@gmail.com>
2023-06-09 22:20:25 -0600

    Agregar archivos de proyecto.

---
+++ MendozaJader/Cliente.cs
@@ -0,0 +1,72 @@
+﻿namespace MendozaJader
+{
+    public  class Cliente
+    {
+        // prop tabx2 y se crea un atributo
+        public int ID { get; set; }
+        public string? nombre { get; set; }
+        public string? correo { get; set; }
+        public string? username { get; set; }
+        public string? password { get; set; }
+
+
+        public Cliente()
+        {
+            ID = 0;
+            nombre = string.Empty;
+            correo =string.Empty;
+            username = string.Empty;
+            password = string.Empty;
+        }
+
+
+        public void getData()
+        {
+            Console.WriteLine("*** Ingrese los datos del cliente ***");
+            Console.WriteLine();
+
+            Console.WriteLine("Ingrese el ID: ");
+            ID = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el nombre: ");
+            string nombre = Console.ReadLine();
+            this.nombre = nombre;
+
+            Console.WriteLine("Ingrese el nombre de usuario: ");
+            string username = Console.ReadLine();
+            this.username = username;
+
+            Console.WriteLine("Ingrese el correo: ");
+            string correo = Console.ReadLine();
+            this.correo = correo;
+
+
+            Console.WriteLine("Ingrese la contraseña: ");
+            string password = Console.ReadLine();
+            this.password = password;
+
+
+        }
+
+        public void printData()
+        {
+            Console.WriteLine("*** Datos del cliente ***");
+            Console.WriteLine();
+
+            Console.WriteLine("ID: " + ID);
+            Console.WriteLine("Nombre: " + nombre);
+            Console.WriteLine("Nombre de usuario: " + username);
+            Console.WriteLine("Correo: " + correo);
+            Console.WriteLine("Contraseña: " + password);        
+        
+        }
+
+
+    }
+
+
+   
+
+
+
+}
---
+++ MendozaJader/Program.cs
@@ -0,0 +1,14 @@
+﻿using MendozaJader;
+using System.Data;
+
+Cliente Cliente = new Cliente();
+
+
+Cliente.getData();
+
+Console.Clear();
+
+Cliente.printData();
+
+Console.ReadKey();
+
